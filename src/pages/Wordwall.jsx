e743b19a00cc4d62b833b0a731b42ff2a7fc52fa
utamakevin <utama.kevin@yahoo.com>
2023-04-20 14:35:21 +0700

    now with prettier

--- src/pages/Wordwall.jsx
+++ src/pages/Wordwall.jsx
@@ -1,24 +1,26 @@
 import { useState } from "react"
 
 export default function Wordwall() {
-    const [gameStart, setGameStart] = useState(false)
+  const [gameStart, setGameStart] = useState(false)
 
-    const handleClick = () => {
-        setGameStart(true)
-        console.log('click')
-    }
+  const handleClick = () => {
+    setGameStart(true)
+    console.log("click")
+  }
 
-
-    return (
-        <section className="wordwall">
-            <h1>Wordwall</h1>
-            <p>game instruction</p>
-            {gameStart ? (<div className="game">
-                <span>game displayed here</span>
-            </div>)
-            : 
-            (<button onClick={handleClick}>start</button>)}
-            
-        </section>
-    )
+  return (
+    <section className="wordwall">
+      <h1>Wordwall</h1>
+      {gameStart ? (
+        <div className="game">
+          <span>game displayed here</span>
+        </div>
+      ) : (
+        <>
+          <p>game instruction</p>
+          <button onClick={handleClick}>start</button>
+        </>
+      )}
+    </section>
+  )
 }
