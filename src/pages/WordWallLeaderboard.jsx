433b5db95504f21bc87174c13b642f20de7c9d58
utamakevin <utama.kevin@yahoo.com>
2023-04-23 11:05:19 +0700

    db

--- src/pages/WordWallLeaderboard.jsx
+++ src/pages/WordWallLeaderboard.jsx
@@ -1,5 +1,5 @@
-import WordWallLeaderboardModel from "../models/wordWallLeaderboardModel"
-
 export default function WordWallLeaderboard() {
-  WordWallLeaderboardModel.showAll().then(data => console.log(data))
+  return (
+    <h1>Leaderboard</h1>
+  )
 }
